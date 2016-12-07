b874258c67e0edc557a9e98630d706dd4a69ebde
Michael Wanyoike <micwanyoike@gmail.com>
2016-12-07 16:51:14 +0300

    Reorganized code and added documentation

--- fourpicsoneword.js
+++ fourpicsoneword.js
@@ -2,15 +2,24 @@
 
 var fs = require('fs');
 
-//INPUT PARAMETERS
-var targetLength = 6;
-var targetClues = "cclveowsakxi".split("");
+/*****************************************************************************/
+/* INPUT PARAMETERS */
+/*****************************************************************************/
+var targetLength = 6; // LENGTH parameter
+var targetClues = "cclveowsakxi".split(""); // CLUES parameter
 
-//GLOBAL VARIABLES
+/*****************************************************************************/
+/* GLOBAL VARIABLES */
+/*****************************************************************************/
 var alphabet = 'a,b,c,d,e,f,g,h,i,j,k,l,m,o,n,p,q,r,s,t,u,v,w,x,y,z,\'';
 var dictionaries = ['american-english','cracklib-small'];
 var dictionary = dictionaries[1];
-//Load Dictionary
+
+/*****************************************************************************/
+/* BOOTSTRAP */
+/*****************************************************************************/
+
+// Load Dictionary
 var dict = fs.readFileSync(dictionary).toString().split('\n');
 console.log("DICTIONARY [" + dictionary + "] with [" + dict.length + "] WORDS");
 
@@ -18,12 +27,15 @@ console.log("DICTIONARY [" + dictionary + "] with [" + dict.length + "] WORDS");
 console.log("LENGTH PARAMETERS : [" + targetLength + "]\n");
 console.log("CLUES PARAMETER : [" + targetClues + "]\n");
 
-// GENERATE
-var banCharacterList = buildBanCharacterList();
-var targetCluesMap = buildDuplicateCharMap(targetClues);
+// Pre-process data for use by variables
+var banCharacterList = buildBanCharacterList(); // Generate list of alphabet characters not in used in the CLUES parameter
+var targetCluesMap = buildDuplicateCharMap(targetClues); // Convert CLUES parameter into a Map specifying duplicates as an integer value
 
 
-//EXECUTE
+/*****************************************************************************/
+/* RUN */
+/*****************************************************************************/
+
 // Filter Out Words Matching Target Length
 var wordLengthList = matchLength();
 
@@ -40,69 +52,22 @@ console.log("[" + wordCluesList.length + "] Words Passed the Clues Parameter");
 console.log("[" + wordDuplicateList.length + "] Words Passed the Character Duplicate Test");
 
 
-//FUNCTIONS
-function matchCharacterDuplicates(wordList) {
-	var array = new Array();
-	wordList.forEach(function (word) {
-		if(hasValidDuplicates(word))
-			array.push(word);
-	});
-	return array;
-}
+/*****************************************************************************/
+/* UTILITIES */
+/*****************************************************************************/
 
-function hasValidDuplicates(word) {
-	var valid = true;
-	var wordMap = buildDuplicateCharMap(word.split(""));
-	for(const entry of wordMap.entries()) {
-		// Check if a certain character has not been used more than the allowed limit
-		if(entry[1] > targetCluesMap.get(entry[0]))
-			valid = false;
-	}
-	return valid;
-}
-
+//Print Map keys and values to console
 function printMap(map){
 	for(const entry of map.entries()) {
 		console.log(entry[0] + " : " + entry[1]);
 	}
 }
 
+// Print array values to console
 function printArray(array) {
 	array.forEach(function(word){
 		console.log(word);
 	});
-}
-
-function matchLength() {
-	var array = new Array();
-	dict.forEach(function(line) {
-		if(line.trim().length == targetLength) {
-			array.push(line.toLowerCase());
-		}
-	})
-	return array;
-}
-
-function matchClues(wordList) {	
-	//Filter out words using Banned Characters
-	var array = new Array();
-	wordList.forEach(function(word) {
-		if(isWordValid(word)) {
-			array.push(word);
-		}
-	});
-	return array;
-}
-
-//Checks if a word has character in the ban list
-function isWordValid(word) {
-	var valid = true;
-	banCharacterList.forEach(function(banchar) {
-		if(word.indexOf(banchar) > -1){
-			valid = false;
-		}
-	});
-	return valid;
 }
 
 // Generate List of Characters Not Allowed In Filtered Out Words
@@ -116,6 +81,7 @@ function buildBanCharacterList() {
 	return banList;
 }
 
+// Validate if the character provided is in the CLUES parameter
 function isCharInClue(char) {
 	var valid = false;
 	targetClues.forEach(function(clueChar) {
@@ -125,9 +91,11 @@ function isCharInClue(char) {
 	return valid;
 }
 
-function buildDuplicateCharMap(array) {
+// Convert a character array into a Map specifying characters as keys and duplicates as values
+// @charArray parameter - character
+function buildDuplicateCharMap(charArray) {
 	var duplicateCharMap = new Map();
-	array.forEach(function (char) {
+	charArray.forEach(function (char) {
 		if(duplicateCharMap.has(char)) {
 			var val = duplicateCharMap.get(char) + 1;
 			duplicateCharMap.delete(char);
@@ -137,4 +105,63 @@ function buildDuplicateCharMap(array) {
 		}
 	});
 	return duplicateCharMap;
+}
+
+/*****************************************************************************/
+/* FILTERS */
+/*****************************************************************************/
+
+// FILTER ONE : Collect words from the Dictionary that only match the LENGTH Parameter
+function matchLength() {
+	var array = new Array();
+	dict.forEach(function(line) {
+		if(line.trim().length == targetLength) {
+			array.push(line.toLowerCase());
+		}
+	})
+	return array;
+}
+
+// FILTER TWO : Collect words from Filter One that contain characters only included in the CLUES Parameter
+function matchClues(wordList) {
+	var array = new Array();
+	wordList.forEach(function(word) {
+		if(isWordValid(word)) {
+			array.push(word);
+		}
+	});
+	return array;
+}
+
+// Invalidate words that contain characters not provided in the CLUES parameter
+function isWordValid(word) {
+	var valid = true;
+	banCharacterList.forEach(function(banchar) {
+		if(word.indexOf(banchar) > -1){
+			valid = false;
+		}
+	});
+	return valid;
+}
+
+// FILTER THREE : Collect words from Filter 2 list that have not exceeded the duplicate characters specified in the CLUES parameter
+function matchCharacterDuplicates(wordList) {
+	var array = new Array();
+	wordList.forEach(function (word) {
+		if(hasValidDuplicates(word))
+			array.push(word);
+	});
+	return array;
+}
+
+// Compare the number of duplicates in each character with the number of duplicates in the CLUES parameter
+function hasValidDuplicates(word) {
+	var valid = true;
+	var wordMap = buildDuplicateCharMap(word.split(""));
+	for(const entry of wordMap.entries()) {
+		// Check if a certain character has not been used more than the allowed limit
+		if(entry[1] > targetCluesMap.get(entry[0]))
+			valid = false;
+	}
+	return valid;
 }
